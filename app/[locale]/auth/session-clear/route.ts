a6b5861e5fecc2c920b8ee64aa59e627c7775fb2
Ylya <ylyamartchenko@gmail.com>
2026-01-15 23:34:58 +0100

    cookies consent

--- app/[locale]/auth/session-clear/route.ts
+++ app/[locale]/auth/session-clear/route.ts
@@ -1,7 +1,8 @@
 import { deleteUserSessionCookies } from "@/utils/cookies/cookies.server";
 import { redirect } from "next/navigation";
+import { ERROR_CODES } from "@/utils/errors.utils";
 
 export async function GET() {
   await deleteUserSessionCookies();
-  redirect("/auth");
+  redirect("/auth?error=" + ERROR_CODES.AUTH[4]);
 }
